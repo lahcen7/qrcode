2bbf93892cb4a51e08f2b18cb6e9e45238b835ae
llachgar <llachgar@student.1337.ma>
2021-01-24 11:41:59 +0000

    add drawer

--- lib/pages/scan.dart
+++ lib/pages/scan.dart
@@ -22,6 +22,37 @@ class ScanPageState extends State<ScanPage> {
   String result = "a";
   bool resultScanned = false;
   TapGestureRecognizer _flutterTapRecognizer;
+
+
+
+  // DRAWER CODE
+    Offset _offset = Offset(0,0);
+  GlobalKey globalKey = GlobalKey();
+  List<double> limits = [];
+
+  bool isMenuOpen = false;
+
+  getPosition(duration){
+    RenderBox renderBox = globalKey.currentContext.findRenderObject();
+    final position = renderBox.localToGlobal(Offset.zero);
+    double start = position.dy - 20;
+    double contLimit = position.dy + renderBox.size.height - 20;
+    double step = (contLimit-start)/5;
+    limits = [];
+    for (double x = start; x <= contLimit; x = x + step) {
+      limits.add(x);
+    }
+    setState(() {
+      limits = limits;
+    });
+
+  }
+
+  double getSize(int x){
+    double size  = (_offset.dy > limits[x] && _offset.dy < limits[x + 1]) ? 25 : 20;
+    return size;
+  }
+  // DRAWER CODE END 
   Future _scanQR() async {
     try {
       String qrResult = await BarcodeScanner.scan();
@@ -51,6 +82,9 @@ class ScanPageState extends State<ScanPage> {
 
   @override
   void initState() {
+     limits= [0, 0, 0, 0, 0, 0];
+    WidgetsBinding.instance.addPostFrameCallback(getPosition);
+
     super.initState();
     _flutterTapRecognizer = new TapGestureRecognizer()
       ..onTap = () => _openUrl(result);
@@ -80,6 +114,10 @@ class ScanPageState extends State<ScanPage> {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<DynamicTheme>(context);
+    Size mediaQuery = MediaQuery.of(context).size;
+    double sidebarSize = mediaQuery.width * 0.65;
+    double menuContainerHeight = mediaQuery.height/2;
+
     if (result != "a") {
       Clipboard.setData(
         ClipboardData(text: result),
@@ -97,79 +135,26 @@ class ScanPageState extends State<ScanPage> {
       //   resultScanned = false;
       // });
     }
-    return Scaffold(
-      key: _scaffoldKey,
-      drawer: Drawer(
-        elevation: 0,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              child: Text('QR CODE SCANNER BY: LAHCEN LACHGAR'),
+    return SafeArea(
+        child: Scaffold(
+          floatingActionButton: FloatingActionButton.extended(
+        icon: Icon(Icons.camera_alt),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+            body: Container(
               decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Color.fromARGB(255, 60, 140, 231),
-                    Color.fromARGB(255, 0, 234, 255),
-                  ],
-                ),
+                  gradient: LinearGradient(colors: [
+                    Color.fromRGBO(255, 65, 108, 1.0),
+                    Color.fromRGBO(255, 75, 73, 1.0)
+                  ])
               ),
-            ),
-            Divider(
-              height: 2.0,
-            ),
-            Builder(
-              builder: (context) => ListTile(
-                title: Text('Toggle Dark mode'),
-                leading: Icon(Icons.brightness_4),
-                onTap: () {
-                  setState(() {
-                    themeProvider.changeDarkMode(!themeProvider.isDarkMode);
-                  });
-                  Navigator.pop(context);
-                },
-                trailing: CupertinoSwitch(
-                  value: themeProvider.getDarkMode(),
-                  onChanged: (value) {
-                    setState(() {
-                      themeProvider.changeDarkMode(value);
-                    });
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-            ),
-            Divider(
-              height: 2.0,
-            ),
-            Builder(
-              builder: (context) => ListTile(
-                leading: Icon(Icons.open_in_browser),
-                title: new InkWell(
-                    child: Text('Visit my website!'),
-                    onTap: () {
-                      launch('http://codenameakshay.tech');
-                      Navigator.pop(context);
-                    }),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-            Divider(
-              height: 2.0,
-            ),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text("QR Scan"),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Center(
+              width: mediaQuery.width,
+              child: Stack(
+                children: <Widget>[
+                  Center(
             child: Text(
               "Press scan to scan barcodes or QR codes.",
               style: new TextStyle(
@@ -215,14 +200,190 @@ class ScanPageState extends State<ScanPage> {
                   ],
                 )
               : Container(),
+                  AnimatedPositioned(
+                    duration: Duration(milliseconds: 1500),
+                    left: isMenuOpen?0: -sidebarSize+20,
+                    top: 0,
+                    curve: Curves.elasticOut,
+                    child: SizedBox(
+                      width: sidebarSize,
+                      child: GestureDetector(
+                        onPanUpdate: (details){
+                          if(details.localPosition.dx <=sidebarSize){
+                            setState(() {
+                              _offset = details.localPosition;
+                            });
+                          }
+
+                          if(details.localPosition.dx>sidebarSize-20 && details.delta.distanceSquared>2){
+                            setState(() {
+                              isMenuOpen = true;
+                            });
+                          }
+
+                        },
+                        onPanEnd: (details){
+                           setState(() {
+                             _offset = Offset(0,0);
+                           });
+                        },
+                        child: Stack(
+                          children: <Widget>[
+                            CustomPaint(
+                              size: Size(sidebarSize, mediaQuery.height),
+                              painter: DrawerPainter(offset: _offset),
+                            ),
+                            Container(
+                              height: mediaQuery.height,
+                              width: sidebarSize,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.max,
+                                children: <Widget>[
+                                  Container(
+                                    height: mediaQuery.height*0.25,
+                                    child: Center(
+                                      child: Column(
+                                        children: <Widget>[
+                                          Image.asset("assets/dp_default.png",width: sidebarSize/2,),
+                                          Text("LACHGAR LAHCEN",style: TextStyle(color: Colors.black45),),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Divider(thickness: 1,),
+                                  Container(
+                                    key: globalKey,
+                                    width: double.infinity,
+                                    height: menuContainerHeight,
+                                    child: Column(
+                                      children: <Widget>[
+                                        MyButton(
+                                          text: "Profile",
+                                          iconData: Icons.person,
+                                          textSize: getSize(0),
+                                          height: (menuContainerHeight)/5,
+                                        ),
+                                        MyButton(
+                                          text: "Payments",
+                                          iconData: Icons.payment,
+                                          textSize: getSize(1),
+                                          height: (menuContainerHeight)/5,),
+                                        MyButton(
+                                          text: "Notifications",
+                                          iconData: Icons.notifications,
+                                          textSize: getSize(2),
+                                          height: (mediaQuery.height/2)/5,),
+                                        MyButton(
+                                          text: "Settings",
+                                          iconData: Icons.settings,
+                                          textSize: getSize(3),
+                                          height: (menuContainerHeight)/5,),
+                                        MyButton(
+                                          text: "My Files",
+                                          iconData: Icons.attach_file,
+                                          textSize: getSize(4),
+                                          height: (menuContainerHeight)/5,),
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            AnimatedPositioned(
+                              duration: Duration(milliseconds: 400),
+                              right: (isMenuOpen)?10:sidebarSize,
+                              bottom: 30,
+                              child: IconButton(
+                                enableFeedback: true,
+                                icon: Icon(Icons.keyboard_backspace,color: Colors.black45,size: 30,),
+                                onPressed: (){
+                                  this.setState(() {
+                                    isMenuOpen = false;
+                                  });
+                                },),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+        )); 
+  }
+}
+
+// REFACTORED
+class MyButton extends StatelessWidget {
+  final String text;
+  final IconData iconData;
+  final double textSize;
+  final double height;
+
+  MyButton({this.text, this.iconData, this.textSize,this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialButton(
+      height: height,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Icon(
+            iconData,
+            color: Colors.black45,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            text,
+            style: TextStyle(color: Colors.black45, fontSize: textSize),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.camera_alt),
-        label: Text("Scan"),
-        onPressed: _scanQR,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      onPressed: () {},
     );
   }
+}
+
+// REFACTRORED
+class DrawerPainter extends CustomPainter{
+
+  final Offset offset;
+
+  DrawerPainter({this.offset});
+
+  double getControlPointX(double width){
+    if(offset.dx == 0){
+      return width;
+    } else {
+      return offset.dx>width?offset.dx:width+75;
+    }
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()..color = Colors.white..style = PaintingStyle.fill;
+    Path path = Path();
+    path.moveTo(-size.width, 0);
+    path.lineTo(size.width, 0);
+    path.quadraticBezierTo(getControlPointX(size.width), offset.dy, size.width, size.height);
+    path.lineTo(-size.width, size.height);
+    path.close();
+
+    canvas.drawPath(path, paint);
+
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
+  }
+
 }
